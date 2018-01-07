a5c7a9d7eaf80bc698850a8bd761c09e8e777fa9
jsirianni <jsirianni@ws1.sir.local>
2018-01-06 21:39:00 -0500

    use arguments instead of example directories and bucket names

--- team-backup-freenas.sh
+++ team-backup-freenas.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 0.2.0
+# Version 0.2.1
 
 # Rclone backup script
 # Run from an hourly cron
@@ -7,18 +7,23 @@
 # Copy mode will copy but not delete (use sync if deletion is desired)
 # 8 transfers at a time (default is 4)
 # Will run at a max of 5mbps (0.625) during working (8am to 6pm) hours
-# --fast-list 
+# --fast-list
 # Verbose output is sent to /var/log/syslog
+
+# Arguments
+# $1 = source (directory or remote_name:bucket_name)
+# $2 = destination (directory or remote_name:bucket_name)
+# Example
+# ./team-backup-freenas.sh /mnt/data myremotename:bucketname
 
 if pgrep rclone; then
 echo "backup already running"
-logger Rclone tried to backup, already running. 
+logger Rclone tried to backup, already running.
 exit 1
 fi
 
-
 logger Rclone backup started
-/usr/local/bin/rclone copy /mnt/Data/companyfiles vm-companyfiles:vm-companyfiles \
+/usr/local/bin/rclone copy $1 $2 \
 --transfers=8 \
 --buffer-size=25M \
 --bwlimit "08:00,0.25M 18:00,0.625M" \
--- team-backup.sh
+++ team-backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 0.2.0
+# Version 0.2.1
 
 # Rclone backup script
 # Run from an hourly cron
@@ -7,16 +7,23 @@
 # Copy mode will copy but not delete (use sync if deletion is desired)
 # 8 transfers at a time (default is 4)
 # Will run at a max of 5mbps (0.625) during working (8am to 6pm) hours
-# --fast-list 
+# --fast-list
 # Verbose output is sent to /var/log/syslog
+
+# Arguments
+# $1 = source (directory or remote_name:bucket_name)
+# $2 = destination (directory or remote_name:bucket_name)
+# Example
+# ./team-backup.sh /mnt/data myremotename:bucketname
 
 if pidof -o %PPID -x "team-backup.sh"; then
 echo "backup already running"
-logger Rclone tried to backup, already running. 
+logger Rclone tried to backup, already running.
 exit 1
 fi
+
 logger Rclone backup started
-rclone copy /<source> <remote_name>:<bucket_name> \
+rclone copy $1 $2 \
 --transfers=8 \
 --buffer-size=25M \
 --bwlimit "08:00,0.625M 18:00,off" \
